7d5c1bb226dc167fb2b7362c269c92cea5fac408
nick-romano <naromano22@gmail.com>
2018-03-21 14:57:10 -0400

    configured hot server for dev, typescript transpiler, more

---
+++ app/main.js
@@ -0,0 +1,18 @@
+define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], function (require, exports, EsriMap, MapView, FeatureLayer) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var map = new EsriMap({
+        basemap: "streets"
+    });
+    var view = new MapView({
+        map: map,
+        container: "viewDiv",
+        center: [-76.93, 38.9897],
+        zoom: 12
+    });
+    view.when(function () {
+        var layer = new FeatureLayer({ url: "http://maps.umd.edu/arcgis/rest/services/Layers/Transportation/MapServer/4" });
+        console.log('it _works');
+    });
+});
+//# sourceMappingURL=main.js.map
---
+++ devServer.js
@@ -0,0 +1,58 @@
+var express = require('express')
+var http = require('http')
+var path = require('path')
+var reload = require('reload')
+var bodyParser = require('body-parser')
+var logger = require('morgan')
+var watch = require('watch')
+var sockjs = require('sockjs');
+var http = require('http');
+
+var app = express();
+
+var sockjs_opts = { sockjs_url: "http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js" };
+var sockjs_echo = sockjs.createServer(sockjs_opts);
+sockjs_echo.on('connection', function (conn) {
+    conn.on('data', function (message) {
+        conn.write(message);
+        watch.createMonitor(__dirname + "/app", function (monitor) {
+            monitor.files['main.js'] // Stat object for my zshrc.
+            monitor.on("created", function (f, stat) {
+                // Handle new files
+            })
+            monitor.on("changed", function (f, curr, prev) {
+                // Handle file changes
+                console.log("a file has changed")
+                conn.write('reload')
+            })
+            monitor.on("removed", function (f, stat) {
+
+            })
+        })
+    });
+});
+
+
+var publicDir = path.join(__dirname, 'app')
+
+app.set('port', process.env.PORT || 3000)
+app.use(logger('dev'))
+app.use(bodyParser.json()) // Parses json, multi-part (file), url-encoded 
+
+app.use(express.static('app'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(publicDir, 'index.html'))
+})
+
+var server = http.createServer(app)
+
+reloadServer = reload(app);
+
+
+sockjs_echo.installHandlers(server, { prefix: '/echo' });
+
+
+server.listen(app.get('port'), function () {
+    console.log('Web server listening on port ' + app.get('port'))
+})
+
