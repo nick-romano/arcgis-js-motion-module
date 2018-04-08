eb749c37f712ec28df65dbd44fc53ada24d2d22f
nick-romano <naromano22@gmail.com>
2018-04-08 09:17:24 -0400

    added setters for _LayerLines & _LayerPoints

--- app/motion-module.ts
+++ app/motion-module.ts
@@ -6,56 +6,73 @@ import { Point, Polygon, Polyline } from "esri/geometry";
 import { subclass, property, declared } from "esri/core/accessorSupport/decorators";
 
 
-
 @subclass("esri/layers/MotionLayer")
 class MotionLayer extends declared(Layer) {
     @property()
     source: object;
 
     @property()
+    _LayerLines: object;
     _LayerPoints: object;
+    
 
+    constructor(args: object){
+        super()
+        this.LayerLines = args["source"];
+        this.LayerPoints = args["source"];
+    }
+
+    get LayerLines(): object {
+        return this._LayerLines
+    }
+
+    set LayerLines(value:object) {
+        const lineSymbol = new SimpleLineSymbol({
+            color: [226, 119, 40],  // RGB color values as an array
+            width: 4
+        });
+
+        var geom = value["data"].features.map((r:any) => r.geometry);
+        geom.paths = geom.map((r: any) => {
+            var a = r.coordinates.map((a: any) => a)
+            return a
+        });
+
+        var LineFeatures = geom.filter((r: any) => r.type === "LineString" ? r : null);
+        LineFeatures.map((r: any) => {
+            r.geometry = new Polyline();
+            r.type = "polyline";
+            r.attributes = { a: "b" };
+            r.geometry.paths[0] = [];
+            r.coordinates.map((t: object) => r.geometry.paths[0].push([t[0], t[1]]));
+            r.symbol = lineSymbol;
+            r.graphic = new Graphic({ geometry: r.geometry, attributes: r.attributes, symbol: r.symbol });
+        });
+
+        this._LayerLines = LineFeatures
+    }
 
     get features(): object {
-        var geom = this.source.data.features.map(r => r.geometry);
-        geom.paths = geom.map(r => {
-            var a = r.coordinates.map(a => a)
+        var geom = this.source["data"].features.map(r => r.geometry);
+        geom.paths = geom.map((r: any) => {
+            var a = r.coordinates.map((a: any) => a)
             return a
         });
         return geom
     }
 
     get LayerPoints(): object {
-        var PointFeatures = this.features.filter(r => r.type === "Point" ? r : null);
-        PointFeatures.Points = new Array;
-        PointFeatures.map(r => r.geometry = new Point(r.coordinates));
-        return PointFeatures
+        return this._LayerPoints;
     }
 
-    get LayerLines(): object {
-        const lineSymbol = new SimpleLineSymbol({
-            color: [226, 119, 40],  // RGB color values as an array
-            width: 4
-          });
+    set LayerPoints(value: object) {
+        var geom = value["data"].features.map((r:any) => r.geometry);
 
-          var geom = this.source.data.features.map(r => r.geometry);
-          geom.paths = geom.map(r => {
-              var a = r.coordinates.map(a => a)
-              return a
-          });
+        var PointFeatures = geom.filter((r:any) => r.type === "Point" ? r: null);
+        PointFeatures.Points = new Array;
+        PointFeatures.map((r: any) => r.geometry = new Point(r.coordinates));
 
-        var LineFeatures = geom.filter(r => r.type === "LineString" ? r : null);
-        LineFeatures.map(r => {
-            r.geometry = new Polyline();
-            r.type = "polyline";
-            r.attributes = {a: "b"}
-            r.coordinates.map(t => r.geometry.paths.push([t[0], t[1]]));
-            r.symbol = lineSymbol;
-            r.graphic = new Graphic({geometry: r.geometry, attributes: r.attributes, symbol: r.symbol});
-        });
-
-
-        return LineFeatures
+        this._LayerPoints = PointFeatures;
     }
 }
 
